dbcc2d3aa23a246e68186e28cce7d5f2f22d906b
sLucas27 <32438762+sLucas27@users.noreply.github.com>
2021-02-13 21:17:02 +0100

    Completed exercise 1: MessageToTheWorld.
    Completed exercise 1: MessageToTheWorld.

---
+++ src/lab1/MessageToTheWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author lucas
+ */
+public class MessageToTheWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("My name is Lucas, and I’m a humble man.");
+        
+    }
+    
+}
